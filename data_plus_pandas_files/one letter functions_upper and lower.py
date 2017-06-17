c83605b43fad497938f4aaa4c97cbe0053099036
Felienne <felienne@gmail.com>
2017-06-17 10:26:53 +0200

    added function example to the intro

---
+++ data_plus_pandas_files/one letter functions_upper and lower.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+
+
+df = pd.read_csv('output/distributions of one letter functions_upper and lower.csv')
+
+
+df.plot(kind='bar', stacked=True)
+
+
+
+
+
